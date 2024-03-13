ee7da6967f0faf63207133b083334d5d3676ed52
RomoDavid21 <romo.david@uabc.edu.mx>
2024-03-12 19:12:13 -0700

    Practica 18 David Romo

---
+++ Practica 18/index.js
@@ -0,0 +1,13 @@
+var dias = [];
+
+alert("Ingrese los dias de la semana en orden.")
+
+dias[1] = prompt("Ingreses dia:")
+dias[2] = prompt("Ingreses dia:")
+dias[3] = prompt("Ingreses dia:")
+dias[4] = prompt("Ingreses dia:")
+dias[5] = prompt("Ingreses dia:")
+dias[6] = prompt("Ingreses dia:")
+dias[7] = prompt("Ingreses dia:")
+
+alert(dias)
