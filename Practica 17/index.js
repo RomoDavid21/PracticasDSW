79ffa681235f7f2161928fa11bf79dbd95d74fbe
RomoDavid21 <romo.david@uabc.edu.mx>
2024-03-12 19:00:24 -0700

    Practica 17 David Romo

---
+++ Practica 17/index.js
@@ -0,0 +1,19 @@
+var num1 = 10;
+var num2 = 5;
+var num3;
+var num4;
+
+num3 = prompt("Ingrese un numero")
+num4 = prompt("Ingrese un segundo numero:")
+
+suma = num1 + num2;
+resta = num1 - num2;
+multi = num1 * num2;
+div = num1 / num2;
+
+suma2 = num3 + num4;
+resta2 = num3 - num4;
+multi2 = num3 * num4;
+div2 = num3 / num4;
+
+alert("Datos Constantes\nLa suma es: "+suma+"\nLa resta es: "+resta+"\nLa multipliacion es: "+multi+"\nLa division es: "+div+"\n\nDatos Ingresados\nLa suma es: "+suma2+"\nLa resta es: "+resta2+"\nLa multipliacion es: "+multi2+"\nLa division es: "+div2)
